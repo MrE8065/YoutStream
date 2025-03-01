daea2dda40c393fb6cc4524dfe2570be9dd81828
MrE8065 <102242666+MrE8065@users.noreply.github.com>
2025-03-01 14:32:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,62 +1,74 @@
 import flet as ft
-from youtube import obtener_url_audio, reproducir_audio, obtener_miniatura
+from youtube import obtener_url_audio, reproducir_audio, obtener_miniatura, obtener_titulo
 
 def main(page: ft.Page):
     page.title = "YoutStream"
     page.window.resizable = False
     page.window.width = 400
-    page.window.height = 600
+    page.window.height = 400
     page.window.alignment = ft.alignment.bottom_right
     
     image = ft.Image(src=None, visible=False)
     placeholder_text = ft.Text("A la espera de un stream...", visible=True)
     
-    def update_image(e):
+    def get_stream(e):
         url = url_input.value
         if url:
-            image.src = obtener_miniatura(url)
-            image.visible = True
-            image.update()
-            placeholder_text.visible = False
-            placeholder_text.update()
-        page.close(dialog)
+            image.src = obtener_miniatura(url) # Obtener la miniatura del video
+            image.visible = True # Mostrar la imagen
+            placeholder_text.visible = False # Esconder el placeholder
+            titulo = obtener_titulo(url)  # Obtener el título del video
+            page.title = titulo  # Cambiar el título de la ventana
+        page.close(dialog) # Cierra el diálogo
+        page.update() # Actualiza la página para mostrar los cambios
     
-    url_input = ft.TextField(hint_text="Stream de Youtube...", on_submit=update_image)
+    url_input = ft.TextField(hint_text="Stream de Youtube...", on_submit=get_stream)
+    
+    dialog_button = ft.IconButton(icon=ft.Icons.MENU_ROUNDED, on_click=lambda e: page.open(dialog))
     
     dialog = ft.AlertDialog(
         modal=True,
         title=ft.Text("Selecciona un stream"),
         content=url_input,
         actions=[
-            ft.TextButton("Aceptar", on_click=update_image),
+            ft.TextButton("Aceptar", on_click=get_stream),
             ft.TextButton("Cancelar", on_click=lambda e: page.close(dialog))
         ],
-        actions_alignment=ft.MainAxisAlignment.END,
+        actions_alignment=ft.MainAxisAlignment.END
     )
+    
+    topbar = ft.Row(
+        controls=[dialog_button],
+        alignment=ft.MainAxisAlignment.END
+    )
+    
+    control_buttons=ft.Row(
+        controls=[
+            #ft.IconButton(icon=ft.Icons.SKIP_PREVIOUS_ROUNDED, icon_size=35),
+            ft.IconButton(icon=ft.Icons.PLAY_CIRCLE_FILL_ROUNDED, icon_size=35),
+            #ft.IconButton(icon=ft.Icons.SKIP_NEXT_ROUNDED, icon_size=35)
+        ]
+    )
+    
+    bottom_buttons=ft.Row(
+        controls=[control_buttons],
+        alignment=ft.MainAxisAlignment.CENTER
+    )
+    
     
     things = ft.Container(
         content=ft.Column(
             controls=[
-                ft.Text("Nombre del stream"),
-                ft.ElevatedButton("Abrir diálogo", on_click=lambda e: page.open(dialog)),
+                topbar,
                 placeholder_text,
                 image,
-                ft.Row(
-                    controls=[
-                        ft.IconButton(icon=ft.Icons.SKIP_PREVIOUS_ROUNDED),
-                        ft.IconButton(icon=ft.Icons.PLAY_CIRCLE_FILL_ROUNDED),
-                        ft.IconButton(icon=ft.Icons.SKIP_NEXT_ROUNDED),
-                    ],
-                    alignment=ft.MainAxisAlignment.CENTER
-                )
+                bottom_buttons
             ],
             horizontal_alignment=ft.CrossAxisAlignment.CENTER,
             alignment=ft.MainAxisAlignment.SPACE_BETWEEN
         ),
         alignment=ft.alignment.center,
         expand=True,
-        padding=10,
-        border=ft.border.all(1, ft.Colors.GREEN)
     )
     
     page.add(things)
